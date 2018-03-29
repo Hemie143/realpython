14ca02f4ece329b679eecee20b532efa34eb9ac0
Laurent Hemeryck <hemie143@gmail.com>
2018-03-29 15:45:31 +0200

    invest

--- chp05 - Functions and Loops/invest.py
+++ chp05 - Functions and Loops/invest.py
@@ -1,3 +1,11 @@
 
+def invest(amount, rate, time):
+    print('principal amount: ${}'.format(amount))
+    print('annual rate or return: {}'.format(rate))
+    cap = amount
+    for y in range(time):
+        amount *= (1+rate)
+        print('year {}: ${}'.format(y+1, amount))
 
-
+invest(100, .05, 8)
+invest(2000, .025, 5)
