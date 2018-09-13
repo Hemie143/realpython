db7723a48292d15aefe57e46a4d6c09aaeb3b860
Laurent Hemeryck <hemie143@gmail.com>
2018-09-13 17:37:00 +0200

    Renaming chapter numbers

---
+++ chp04 - Strings and Methods/04-01.py
@@ -0,0 +1,7 @@
+print('double quote "" example')
+print("single quote ' example")
+print("""this is a
+        string that spans across multiple lines
+        that also preserves whitepace.""")
+print("Here's a string that I want to \
+write across multiple lines since it is long.")
---
+++ chp04 - Strings and Methods/04-03.py
@@ -0,0 +1,2 @@
+user_input = input("Hey, what's up? ")
+print("You said: ", user_input)
